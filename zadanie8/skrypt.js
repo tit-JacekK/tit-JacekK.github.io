0c1f07061819fd60ff8f76cecf73f029263bebd6
tit-JacekK <64531784+tit-JacekK@users.noreply.github.com>
2020-05-27 01:35:32 +0200

    Add files via upload

---
+++ zadanie8/skrypt.js
@@ -0,0 +1,72 @@
+function zmien1()
+{
+	document.getElementById('1').src="2.jpg";
+}
+function zmien2()
+{
+	document.getElementById('1').src="1.jpg";
+}
+function zmien3()
+{
+	document.getElementById('2').src="4.jpg";
+}
+function zmien4()
+{
+	document.getElementById('2').src="3.jpg";
+}
+function zmien5()
+{
+	document.getElementById('3').src="6.jpg";
+}
+function zmien6()
+{
+	document.getElementById('3').src="5.jpg";
+}
+function zmien7()
+{
+	document.getElementById('4').src="8.jpg";
+}
+function zmien8()
+{
+	document.getElementById('4').src="7.jpg";
+}
+function zmien9()
+{
+	document.getElementById('5').src="10.jpg";
+}
+function zmien10()
+{
+	document.getElementById('5').src="9.jpg";
+}
+function zmien11()
+{
+	document.getElementById('6').src="12.jpg";
+}
+function zmien12()
+{
+	document.getElementById('6').src="11.jpg";
+}
+function zmien13()
+{
+	document.getElementById('7').src="14.jpg";
+}
+function zmien14()
+{
+	document.getElementById('7').src="13.jpg";
+}
+function zmien15()
+{
+	document.getElementById('8').src="16.jpg";
+}
+function zmien16()
+{
+	document.getElementById('8').src="15.jpg";
+}
+function zmien17()
+{
+	document.getElementById('9').src="18.jpg";
+}
+function zmien18()
+{
+	document.getElementById('9').src="17.jpg";
+}
