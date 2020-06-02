4cd8878b0efec240907e888465dcd3d15170195a
tit-JacekK <64531784+tit-JacekK@users.noreply.github.com>
2020-06-02 20:00:33 +0200

    Add files via upload

---
+++ zadanie9/skrypt.js
@@ -0,0 +1,168 @@
+function zmien1()
+{
+	document.getElementById('1').src="2.jpg";
+}
+function zmien2()
+{
+	document.getElementById('1').src="1.jpg";
+}
+function zmien3()
+{
+	document.getElementById('2').src="4.jpg";
+}
+function zmien4()
+{
+	document.getElementById('2').src="3.jpg";
+}
+function zmien5()
+{
+	document.getElementById('3').src="6.jpg";
+}
+function zmien6()
+{
+	document.getElementById('3').src="5.jpg";
+}
+function zmien7()
+{
+	document.getElementById('4').src="8.jpg";
+}
+function zmien8()
+{
+	document.getElementById('4').src="7.jpg";
+}
+function zmien9()
+{
+	document.getElementById('5').src="10.jpg";
+}
+function zmien10()
+{
+	document.getElementById('5').src="9.jpg";
+}
+function zmien11()
+{
+	document.getElementById('6').src="12.jpg";
+}
+function zmien12()
+{
+	document.getElementById('6').src="11.jpg";
+}
+function zmien13()
+{
+	document.getElementById('7').src="14.jpg";
+}
+function zmien14()
+{
+	document.getElementById('7').src="13.jpg";
+}
+function zmien15()
+{
+	document.getElementById('8').src="16.jpg";
+}
+function zmien16()
+{
+	document.getElementById('8').src="15.jpg";
+}
+function zmien17()
+{
+	document.getElementById('9').src="18.jpg";
+}
+function zmien18()
+{
+	document.getElementById('9').src="17.jpg";
+}
+function zmien19()
+{
+	document.getElementById('10').src="20.jpg";
+}
+function zmien20()
+{
+	document.getElementById('10').src="19.jpg";
+}
+function zmien21()
+{
+	document.getElementById('11').src="22.jpg";
+}
+function zmien22()
+{
+	document.getElementById('11').src="21.jpg";
+}
+function zmien23()
+{
+	document.getElementById('12').src="24.jpg";
+}
+function zmien24()
+{
+	document.getElementById('12').src="23.jpg";
+}
+function zmien25()
+{
+	document.getElementById('13').src="26.jpg";
+}
+function zmien26()
+{
+	document.getElementById('13').src="25.jpg";
+}
+function zmien27()
+{
+	document.getElementById('14').src="28.jpg";
+}
+function zmien28()
+{
+	document.getElementById('14').src="27.jpg";
+}
+function zmien29()
+{
+	document.getElementById('15').src="30.jpg";
+}
+function zmien30()
+{
+	document.getElementById('15').src="29.jpg";
+}
+function zmien31()
+{
+	document.getElementById('16').src="32.jpg";
+}
+function zmien32()
+{
+	document.getElementById('16').src="31.jpg";
+}
+function zmien33()
+{
+	document.getElementById('17').src="34.jpg";
+}
+function zmien34()
+{
+	document.getElementById('17').src="33.jpg";
+}
+function zmien35()
+{
+	document.getElementById('18').src="36.jpg";
+}
+function zmien36()
+{
+	document.getElementById('18').src="35.jpg";
+}
+function zmien37()
+{
+	document.getElementById('19').src="38.jpg";
+}
+function zmien38()
+{
+	document.getElementById('19').src="37.jpg";
+}
+function zmien39()
+{
+	document.getElementById('20').src="40.jpg";
+}
+function zmien40()
+{
+	document.getElementById('20').src="39.jpg";
+}
+function zmien41()
+{
+	document.getElementById('21').src="42.jpg";
+}
+function zmien42()
+{
+	document.getElementById('21').src="41.jpg";
+}
